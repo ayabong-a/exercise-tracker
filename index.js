9c9950587056929644e51a16fb431b1f3fa9d6b1
Athianda Bukula <mtsoloayabonga@gmail.com>
2025-07-14 15:44:49 +0200

    connecting to mongodb, adding models

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(error => {
+    console.error(error);
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
