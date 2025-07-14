947d69c67570261e8283a20e9eb6160817c1557e
Athianda Bukula <mtsoloayabonga@gmail.com>
2025-07-15 00:57:44 +0200

    implementing a functional UI

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static('public'));
 app.use('/api/users', userRoutes);
 
 mongoose.connect(process.env.MONGO_URI, {
